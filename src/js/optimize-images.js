d7eacf0e46e98d27ac4f89075ccf39e62041dd94
Erik Xu <erik_xu@apple.com>
2023-08-30 00:54:14 -0700

    Added image optimizer script

---
+++ src/js/optimize-images.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const path = require('path');
+const sharp = require('sharp');
+
+const inputDir = 'src/public/blog/images/uploads';
+const outputDir = 'dist/blog/images/uploads/';
+
+// Check if the output directory exists, and create it if necessary
+if (!fs.existsSync(outputDir)) {
+  fs.mkdirSync(outputDir, {recursive: true});
+  console.log('Created directory:', outputDir);
+}
+
+function convertToJpg(inputPath, outputPath) {
+  sharp(inputPath)
+    .toFormat('jpg')
+    .toFile(outputPath, (err, info) => {
+      if (err) {
+        console.error('Error converting image:', err);
+      } else {
+        console.log('Image converted:', outputPath);
+      }
+    });
+}
+
+function processDirectory(dirPath, outputSubDir = '') {
+  fs.readdir(dirPath, (err, files) => {
+    if (err) {
+      console.error('Error reading directory:', err);
+      return;
+    }
+
+    files.forEach(file => {
+      const filePath = path.join(dirPath, file);
+      const outputSubDirPath = path.join(outputDir, outputSubDir);
+      const outputFilePath = path.join(
+        outputSubDirPath,
+        file.replace(/\.(jpg|png)$/, '.jpg')
+      );
+
+      fs.stat(filePath, (err, stats) => {
+        if (err) {
+          console.error('Error getting file stats:', err);
+          return;
+        }
+
+        if (stats.isDirectory()) {
+          const subDir = path.join(outputSubDir, file);
+          const subDirPath = path.join(outputDir, subDir);
+          processDirectory(filePath, subDir); // Recursively process sub-directory
+          if (!fs.existsSync(subDirPath)) {
+            fs.mkdirSync(subDirPath, {recursive: true});
+            console.log('Created subdirectory:', subDirPath);
+          }
+        } else if (file.endsWith('.jpg') || file.endsWith('.png')) {
+          convertToJpg(filePath, outputFilePath);
+        }
+      });
+    });
+  });
+}
+
+processDirectory(inputDir);
